e2af6113f5ef24cc36f9090e35625ec35fa858ab
nalin <bistanalin@gmail.com>
2024-02-21 15:18:37 +0545

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
